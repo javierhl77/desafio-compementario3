9d2ff7a26c1d2e23f63650d1ffb812795364c55f
javierhl77 <tupackatari1977@gmail.com>
2024-06-10 11:34:50 -0300

    preciototalCarrito

--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -58,16 +58,23 @@ router.get("/carts/:cid", async (req, res) => {
          console.log("No existe ese carrito con el id");
          return res.status(404).json({ error: "Carrito no encontrado" });
       }
-
-      const productosEnCarrito = carrito.products.map(item => ({
-         product: item.product.toObject(),
-         quantity : item.quantity,
+      let totalCompra = 0;
+      const productosEnCarrito = carrito.products.map(item => {
+        const product = item.product.toObject();
+        const quantity = item.quantity;
+        const totalPrice = product.price * quantity;
+        totalCompra += totalPrice;
+        return {
+         product: { ...product, totalPrice},
+         quantity,
          cartId
-      })
+        };
+         
+      }
    );
 
 
-      res.render("carts", { productos : productosEnCarrito, cartId });
+      res.render("carts", { productos : productosEnCarrito, totalCompra, cartId });
    } catch (error) {
       console.error("Error al obtener el carrito", error);
       res.status(500).json({ error: "Error interno del servidor" });
